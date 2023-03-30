6463a0b64f71979d5bdaaba0ee51227d841834b5
pushedrumex <pushedrumex@naver.com>
2023-03-30 15:19:29 +0900

    test: add dummy data

---
+++ client/src/dummy/dummyPostMany.js
@@ -0,0 +1,16 @@
+const dummy = [
+    {
+      "id": 1,
+      "author": "John Doe",
+      "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      "date": "2022-04-01T12:34:56.789Z"
+    },
+    {
+      "id": 2,
+      "author": "Jane Doe",
+      "title": "Nulla facilisi. Etiam eget nulla at nulla volutpat convallis vitae non nisi.",
+      "date": "2022-03-31T09:12:34.567Z"
+    }
+]
+
+export default dummy;
---
+++ client/src/dummy/dummyPostOne.js
@@ -0,0 +1,8 @@
+const dummy = {
+                "id": 1,
+                "author": "John Doe",
+                "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "date": "2022-04-01T12:34:56.789Z"
+            }
+
+export default dummy;
